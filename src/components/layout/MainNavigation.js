363a1c261e4168fefecee0dfe8605f5341cf5651
ascorbic1230 <nguyenquoctoan1230@gmail.com>
2022-05-19 22:15:27 +0700

    fix header

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,91 +1,94 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from "react-router-dom";
 import LocalStorageUtils from "../../utils/LocalStorageUtils";
 import "./MainNavigation.css";
+
 const MainNavigation = () => {
-  const [user, setUser] = useState();
-  useEffect(() => {
+  const [user, setUser] = useState(LocalStorageUtils.getUser());
+
+  const handleSignOut = () => {
+    LocalStorageUtils.clear();
     setUser(LocalStorageUtils.getUser());
-  }, []);
+  }
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand">
-            HomePage
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <img src="/logo.png" alt="logo" />
+        </Link>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link
+              to="/book-list"
+              className="nav-link"
+            >
+              All Books
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/your-book"
+              className="nav-link"
+            >
+              Your Book
+            </Link>
+          </li>
+        </ul>
+        <ul className="navbar-nav ml-auto mb-lg-0">
+          {user === null ? (
+            <>
               <li className="nav-item">
                 <Link
-                  to="/book-list"
-                  className="nav-link active"
-                  aria-current="page"
+                  to="/signup"
+                  className="nav-link"
                 >
-                  All Books
+                  Sign Up
                 </Link>
               </li>
               <li className="nav-item">
                 <Link
-                  to="/your-book"
-                  className="nav-link active"
-                  aria-current="page"
+                  to="/login"
+                  className="nav-link"
                 >
-                  Your Book
+                  Sign In
                 </Link>
               </li>
-            </ul>
-            <ul className="navbar-nav ml-auto mb-lg-0">
-              {user === null ? (
-                <>
-                  <li className="nav-item">
-                    <Link
-                      to="/signup"
-                      className="nav-link active"
-                      aria-current="page"
-                    >
-                      SignUp
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      to="/login"
-                      className="nav-link active"
-                      aria-current="page"
-                    >
-                      Login
-                    </Link>
-                  </li>
-                </>
-              ) : (
-                <></>
-              )}
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <div
+                  className="nav-link"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </div>
+              </li>
               <li className="nav-item">
                 <Link
                   to="/my-account"
-                  className="nav-link active"
-                  aria-current="page"
+                  className="nav-link"
                 >
                   <Avatar />
                 </Link>
               </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+
+            </>
+          )}
+          <li className="nav-item">
+            <Link
+              to="/cart"
+              className="nav-link"
+            >
+              <ShoppingCartIcon fontSize="large" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
